24056b62aeb70c2f8db0332ca2c912434eb93c57
Alok Hegde <alokhegde221@gmail.com>
2023-03-22 23:07:46 +0530

    completed

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # scrapingInternship
 ## How this works:
 
-It works by using the beautifulSoup Library and a package called requests, all written in python.
+It works by using the <b>beautifulSoup<b> Library and a package called requests, all written in python. Then I have used the headers which basically acts as a get request to get the required data.
 
 ### I have created different functions to get different parts of webpage, such as title, rating, and review count
 
+Next I have all the data stored in a into .csv which is under the name amazon_data.csv
--- scrape1.py
+++ scrape1.py
@@ -1,102 +1,117 @@
 from bs4 import BeautifulSoup
 import requests
-
+import pandas as pd
+import numpy as np
 # Function to extract Product Title
 def get_title(soup):
-	
-	try:
-		# Outer Tag Object
-		title = soup.find("span", attrs={"id":'productTitle'})
 
-		# Inner NavigatableString Object
-		title_value = title.string
+    try:
+        # Outer Tag Object
+        title = soup.find("span", attrs={"id":'productTitle'})
+        
+        # Inner NavigatableString Object
+        title_value = title.text
 
-		# Title as a string value
-		title_string = title_value.strip()
+        # Title as a string value
+        title_string = title_value.strip()
 
-	except AttributeError:
-		title_string = ""	
+    except AttributeError:
+        title_string = ""
 
-	return title_string
+    return title_string
 
 # Function to extract Product Price
 def get_price(soup):
 
-	try:
-		price = soup.find("span", attrs={'id':'priceblock_ourprice'}).string.strip()
+    try:
+        price = soup.find("span", attrs={'id':'priceblock_ourprice'}).string.strip()
 
-	except AttributeError:
+    except AttributeError:
 
-		try:
-			# If there is some deal price
-			price = soup.find("span", attrs={'id':'priceblock_dealprice'}).string.strip()
+        try:
+            # If there is some deal price
+            price = soup.find("span", attrs={'id':'priceblock_dealprice'}).string.strip()
 
-		except:		
-			price = ""	
+        except:
+            price = ""
 
-	return price
+    return price
 
 # Function to extract Product Rating
 def get_rating(soup):
 
-	try:
-		rating = soup.find("i", attrs={'class':'a-icon a-icon-star a-star-4-5'}).string.strip()
-		
-	except AttributeError:
-		
-		try:
-			rating = soup.find("span", attrs={'class':'a-icon-alt'}).string.strip()
-		except:
-			rating = ""	
+    try:
+        rating = soup.find("i", attrs={'class':'a-icon a-icon-star a-star-4-5'}).string.strip()
+    
+    except AttributeError:
+        try:
+            rating = soup.find("span", attrs={'class':'a-icon-alt'}).string.strip()
+        except:
+            rating = ""	
 
-	return rating
+    return rating
 
 # Function to extract Number of User Reviews
 def get_review_count(soup):
-	try:
-		review_count = soup.find("span", attrs={'id':'acrCustomerReviewText'}).string.strip()
-		
-	except AttributeError:
-		review_count = ""	
+    try:
+        review_count = soup.find("span", attrs={'id':'acrCustomerReviewText'}).string.strip()
 
-	return review_count
+    except AttributeError:
+        review_count = ""	
 
+    return review_count
 
+# Function to extract Availability Status
+def get_availability(soup):
+    try:
+        available = soup.find("div", attrs={'id':'availability'})
+        available = available.find("span").string.strip()
+
+    except AttributeError:
+        available = "Not Available"	
+
+    return available
 if __name__ == '__main__':
 
-	# Headers for request
-	HEADERS = ({'User-Agent':
-	            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
-	            'Accept-Language': 'en-US'})
+    # add your user agent 
+    HEADERS = ({'User-Agent':'', 'Accept-Language': 'en-US, en;q=0.5'})
 
-	# The webpage URL
-	URL = "https://www.amazon.in/s?k=bedsheet&crid=22DX57KYTWL8W&sprefix=bedshee%2Caps%2C329&ref=nb_sb_noss_2"
-	
-	# HTTP Request
-	webpage = requests.get(URL, headers=HEADERS)
+    # The webpage URL
+    URL = "https://www.amazon.in/s?k=bags&crid=2M096C61O4MLT&qid=1653308124&sprefix=ba%2Caps%2C283&ref=sr_pg_1"
 
-	# Soup Object containing all data
-	soup = BeautifulSoup(webpage.content, "lxml")
+    # HTTP Request
+    webpage = requests.get(URL, headers=HEADERS)
 
-	# Fetch links as List of Tag Objects
-	links = soup.find_all("a", attrs={'class':'a-link-normal s-no-outline'})
+    # Soup Object containing all data
+    soup = BeautifulSoup(webpage.content, "html.parser")
 
-	# Store the links
-	links_list = []
+    # Fetch links as List of Tag Objects
+    links = soup.find_all("a", attrs={'class':'a-link-normal s-no-outline'})
 
-		# Loop for extracting links from Tag Objects
-	for link in links:
-		links_list.append(link.get('href'))
-		print(links_list.append(link.get('href')))
-	# Loop for extracting product details from each link 
-	for link in links_list:
+    # Store the links
+    links_list = []
 
-		new_webpage = requests.get("https://www.amazon.in" + link, headers=HEADERS)
+    # Loop for extracting links from Tag Objects
+    for link in links:
+            links_list.append(link.get('href'))
 
-		new_soup = BeautifulSoup(new_webpage.content, "lxml")
-		
-		# Function calls to display all necessary product information
-		print("Product Title =", get_title(new_soup))
-		print("Product Price =", get_price(new_soup))
-		print("Product Rating =", get_rating(new_soup))
-		print("Number of Product Reviews =", get_review_count(new_soup))
+    d = {"title":[], "price":[], "rating":[], "reviews":[],"availability":[]}
+    
+    # Loop for extracting product details from each link 
+    for link in links_list:
+        new_webpage = requests.get("https://www.amazon.in" + link, headers=HEADERS)
+
+        new_soup = BeautifulSoup(new_webpage.content, "html.parser")
+
+        # Function calls to display all necessary product information
+        d['title'].append(get_title(new_soup))
+        d['price'].append(get_price(new_soup))
+        d['rating'].append(get_rating(new_soup))
+        d['reviews'].append(get_review_count(new_soup))
+        d['availability'].append(get_availability(new_soup))
+
+    
+    amazon_df = pd.DataFrame.from_dict(d)
+    amazon_df['title'].replace('', np.nan, inplace=True)
+    amazon_df = amazon_df.dropna(subset=['title'])
+    amazon_df.to_csv("amazon_data.csv", header=True, index=False)
